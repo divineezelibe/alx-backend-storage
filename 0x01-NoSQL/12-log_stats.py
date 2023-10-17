347b816c0e085e7263a6a975dad6427f21ec9283
Divine Ezelibe <60388882+divineezelibe@users.noreply.github.com>
2023-10-17 14:22:34 +0100

    Updated Log Stats

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -3,7 +3,6 @@
 Aggregation operations
 """
 from pymongo import MongoClient
-from collections import OrderedDict
 from typing import Tuple
 
 
@@ -14,7 +13,6 @@ def get_nginx_stats() -> Tuple:
         - count of all documents
         - count of each method in the collection
         - count of each GET calls to /status path
-        - count of top 10 visited ips
     """
     client: MongoClient = MongoClient()
     db = client.logs
@@ -27,27 +25,20 @@ def get_nginx_stats() -> Tuple:
     doc_count = collection.estimated_document_count()
     status_path_stats = collection.count_documents({'method': 'GET',
                                                     'path': '/status'})
-    pipeline = [{'$group': {'_id': '$ip', 'count': {'$sum': 1}}},
-                {'$sort': OrderedDict([('count', -1)])},
-                {'$limit': 10}]
-    top_ips = collection.aggregate(pipeline)
     client.close()
-    return doc_count, method_stats, status_path_stats, top_ips
+    return doc_count, method_stats, status_path_stats
 
 
 def print_nginx_stats() -> None:
     """
     Prints stats from nginx query
     """
-    doc_count, method_stats, status_path_stats, top_ips = get_nginx_stats()
+    doc_count, method_stats, status_path_stats = get_nginx_stats()
     print(f'{doc_count} logs')
     print('Methods:')
     for method in method_stats:
         print(f'\tmethod {method.get("method")}: {method.get("count")}')
     print(f'{status_path_stats} status check')
-    print('IPs:')
-    for ip in top_ips:
-        print(f'\t{ip.get("_id")}: {ip.get("count")}')
 
 
 if __name__ == '__main__':
